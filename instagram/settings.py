5a2173419b81949ff650cd20257c924a8e29b9e6
annk1995 <kabuikariuki052@gmail.com>
2022-06-04 16:10:10 +0300

    linking of template

--- instagram/settings.py
+++ instagram/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    'crispy_forms',
+    'post',
+    # 'crispy_forms',
 ]
 
 MIDDLEWARE = [
