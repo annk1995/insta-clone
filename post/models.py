393c98d833a556ee0843a34b0b40568869ba5fab
annk1995 <kabuikariuki052@gmail.com>
2022-06-04 19:12:22 +0300

    added html template

--- post/models.py
+++ post/models.py
@@ -1,5 +1,5 @@
 from audioop import reverse
-from turtle import title
+
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save,post_delete
